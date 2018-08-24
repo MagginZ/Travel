2e92b12b72e75bd5f9492e5a9cba19c5b390d3b8
ZengMeiqi <1350155894@qq.com>
2018-08-24 11:41:19 +0800

    [travel] + project init

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import './assets/styles/reset.css'
+import fastClick from 'fastclick'
 Vue.config.productionTip = false
-
+fastClick.attach(document.body) // 解决移动端三秒延迟问题
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,10 @@
 <template>
-  
+    <div>dad</div>
 </template>
 
 <script>
 export default {
-
+  name: 'Home'
 }
 </script>
 
